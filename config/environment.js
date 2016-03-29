2a123ecbc21b45a94b408d982b49081acc2c57f0
Nicholas Jensen-Hay <NicholasJensenHay@gmail.com>
2016-03-29 08:47:57 -0700

    Add header template and firebase connection

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'bustle',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'http://bustle2.firebaseio.com',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
